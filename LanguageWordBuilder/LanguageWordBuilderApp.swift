35a4a867fd87b52dff4d46f7693d86daed9123b7
Jonathan Gurr <jonathan.golden.gurr@gmail.com>
2020-09-28 06:19:01 -0700

    Initial Commit

---
+++ LanguageWordBuilder/LanguageWordBuilderApp.swift
@@ -0,0 +1,20 @@
+//
+//  LanguageWordBuilderApp.swift
+//  LanguageWordBuilder
+//
+//  Created by Jonathan Gurr on 28-09-20.
+//
+
+import SwiftUI
+
+@main
+struct LanguageWordBuilderApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
