da88359f5b6891a6af21ace4cf074f10cb976963
Jonathan Gurr <jonathan.golden.gurr@gmail.com>
2021-02-08 10:35:15 -0700

    added spanish words

--- LanguageWordBuilder/Languages/SpanishWords.swift
+++ LanguageWordBuilder/Languages/SpanishWords.swift
@@ -8,13 +8,67 @@
 import Foundation
 
 let spanishWordSource = [
+	//easy
 	["hola"], ["hello"],
+	["adiós"], ["goodbye"],
 	["perro"], ["dog"],
 	["gato"], ["cat"],
 	["mujer"], ["woman"],
 	["hombre"], ["man"],
+	["lo siento"], ["sorry"],
+	["gracias"], ["thank you"],
+	["uno"], ["one"],
+	["dos"], ["two"],
+	["tres"], ["three"],
+	["cuatro"], ["four"],
+	["cinco"], ["five"],
+	["seis"], ["six"],
+	["siete"], ["seven"],
+	["ocho"], ["eight"],
+	["nueve"], ["nine"],
+	["diez"], ["ten"],
+	["casa"], ["house"],
+	["cosa"], ["thing"],
+	["bueno"], ["good"],
+	["malo"], ["bad"],
+	["triste"], ["sad"],
+	["feliz"], ["happy"],
+	["carro", "carro/coche/auto"], ["car"],
+	["tren"], ["train"],
+
+
+	//medium
+	["salud"], ["health"],
+	["quién"], ["who"],
+	["qué"], ["what"],
+	["por qué"], ["why"],
+	["porque"], ["because"],
+	["dónde"], ["where"],
+	["cuál"], ["which"],
+	["cómo"], ["how"],
+	["como"], ["like", "like or as"],
+	["palabra"], ["word"],
+	["mal", "no se siente bien"], ["sick"],
+	["aburrido"], ["bored"],
+	["cuidad"], ["city"],
+	["calle"], ["street"],
+	["juguete"], ["toy"],
+	["jugar"], ["to play"],
+	["cantar"], ["to sing"],
+	["ser"], ["to be", "to be (permanent)"],
+	["estar"], ["to be", "to be (temporary)"],
+//	[""], [""],
+
+	
+	//hard
 	["excelentemente"], ["excellently"],
-	["adiós"], ["goodbye"],
+	["ojalá"], ["I hope so"],
+	["ferrocarril"], ["railroad"],
+
+
+	//very hard
+	["ornitorrinco"], ["platypus"],
+
 ]
 
 
