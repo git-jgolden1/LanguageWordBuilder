dd882403b436f2130b88f48ac7c2e411e37f4334
Jonathan Gurr <jonathan.golden.gurr@gmail.com>
2021-03-24 08:10:45 -0700

    appStateDB instance variable bug fixed by adding required argument 'context'

--- LanguageWordBuilder/BackEnd/BackEnd.swift
+++ LanguageWordBuilder/BackEnd/BackEnd.swift
@@ -18,45 +18,46 @@ func setUpBackEndListeners() {
 }
 
 func saveModel() {
-	let appStateDB = AppStateDB()
-	appStateDB.currentAnswer = appState.currentAnswer
-	appStateDB.currentWordIndex = Int32(appState.currentWordIndex)
-	appStateDB.isSelected = ""
-	for b in appState.isSelected {
-		if b {
-			appStateDB.isSelected += "t"
-		} else {
-			appStateDB.isSelected += "f"
-		}
-	}
-	appStateDB.numberOfColumns = Int16(appState.numberOfColumns)
-	appStateDB.score = Int16(appState.score)
-	appStateDB.scrambledLetters = ""
-	for letter in appState.scrambledLetters {
-		appStateDB.scrambledLetters += letter
-	}
-	appStateDB.save()
+	let context = PersistenceController.shared.container.viewContext
+	let appStateDB = AppStateDB(context: context)
+	appStateDB.currentAnswer = "bla"
+//	appStateDB.currentWordIndex = Int32(appState.currentWordIndex)
+//	appStateDB.isSelected = ""
+//	for b in appState.isSelected {
+//		if b {
+//			appStateDB.isSelected += "t"
+//		} else {
+//			appStateDB.isSelected += "f"
+//		}
+//	}
+//	appStateDB.numberOfColumns = Int16(appState.numberOfColumns)
+//	appStateDB.score = Int16(appState.score)
+//	appStateDB.scrambledLetters = ""
+//	for letter in appState.scrambledLetters {
+//		appStateDB.scrambledLetters += letter
+//	}
+//	appStateDB.save()
 }
 
 func loadModel() {
 	let appStateDB = AppStateDB.fetch(context: PersistenceController.shared.context)
 	appState.currentAnswer = appStateDB.currentAnswer
-	appState.currentWordIndex = Int(appStateDB.currentWordIndex)
-	appState.isSelected = [Bool]()
-	for c in appStateDB.isSelected {
-		if c == "t" {
-			appState.isSelected.append(true)
-		} else if c == "f" {
-			appState.isSelected.append(false)
-		} else {
-			print("invalid string value for isSelected appStateDB")
-		}
-	}
-
-	appState.numberOfColumns = Int(appStateDB.numberOfColumns)
-	appState.score = Int(appStateDB.score)
-	appState.scrambledLetters = [String]()
-	for c in appStateDB.scrambledLetters {
-		appState.scrambledLetters.append(String(c))
-  }
+//	appState.currentWordIndex = Int(appStateDB.currentWordIndex)
+//	appState.isSelected = [Bool]()
+//	for c in appStateDB.isSelected {
+//		if c == "t" {
+//			appState.isSelected.append(true)
+//		} else if c == "f" {
+//			appState.isSelected.append(false)
+//		} else {
+//			print("invalid string value for isSelected appStateDB")
+//		}
+//	}
+//
+//	appState.numberOfColumns = Int(appStateDB.numberOfColumns)
+//	appState.score = Int(appStateDB.score)
+//	appState.scrambledLetters = [String]()
+//	for c in appStateDB.scrambledLetters {
+//		appState.scrambledLetters.append(String(c))
+//  }
 }
